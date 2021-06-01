4ac3ff6062e4edfb92d39ad32fbdeef628e33d50
jeanluciano <jeanluciano@mailbox.org>
2021-06-01 00:54:25 -0500

    loading animation andd more tweaks to collection

--- src/containers/Collection/index.js
+++ src/containers/Collection/index.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { CollectionWrapper, Grid, Loading } from "./style";
 import { DataContext } from "contexts/DataContextContainer";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { Logo } from "../../assets/images";
 import {
   LazyLoadImage,
   trackWindowScroll,
@@ -11,54 +12,60 @@ import MetaWrapper from "components/Wrappers/MetaWrapper";
 const Collection = ({ scrollPosition }) => {
   const { contents } = useContext(DataContext);
   const history = useHistory();
-  
-  const [imgsLoaded, setImgsLoaded] = useState(false)
+
+  const [imgsLoaded, setImgsLoaded] = useState(false);
 
   useEffect(() => {
-    const loadImage = image => {
+    const loadImage = (image) => {
       return new Promise((resolve, reject) => {
-        const loadImg = new Image()
-        loadImg.src = image.source
+        const loadImg = new Image();
+        loadImg.src = image.source;
         // wait 2 seconds to simulate loading time
         loadImg.onload = () =>
           setTimeout(() => {
-            resolve(image.source)
-          }, 2000)
+            resolve(image.source);
+          }, 3000);
 
-        loadImg.onerror = err => reject(err)
-      })
-    }
+        loadImg.onerror = (err) => reject(err);
+      });
+    };
 
-    Promise.all(contents.slice(0,16).map(image => loadImage(image)))
+    Promise.all(contents.slice(0, 16).map((image) => loadImage(image)))
       .then(() => setImgsLoaded(true))
-      .catch(err => console.log("Failed to load images", err))
-  }, [])
+      .catch((err) => console.log("Failed to load images", err));
+  }, [contents]);
   return (
     <MetaWrapper>
       <CollectionWrapper>
-        <Loading>
-
-        </Loading>
-        <Grid style={{visibility: imgsLoaded ? "visible": "hidden"  }}>
-          {contents.map((pic, id) => {
-            return (
-              <LazyLoadImage
-                onClick={() =>
-                  history.push(`/gallery/${parseInt(id) + 1}/details`)
-                }
-                visibleByDefault={id< 17}
-                scrollPosition={scrollPosition}
-                effect="blur"
-                src={pic.source}
-                alt={pic.name}
-                threshold="600"
-                width="120"
-                height="120"
-                key={pic.name}
-              />
-            );
-          })}
-        </Grid>
+        {imgsLoaded ? (
+          <Grid>
+            <h2>
+              1111
+            </h2>
+            {contents.map((pic, id) => {
+              return (
+                <LazyLoadImage
+                  onClick={() =>
+                    history.push(`/gallery/${parseInt(id) + 1}/details`)
+                  }
+                  visibleByDefault={id < 17}
+                  scrollPosition={scrollPosition}
+                  effect="blur"
+                  src={pic.source}
+                  alt={pic.name}
+                  threshold="600"
+                  width="120"
+                  height="120"
+                  key={pic.name}
+                />
+              );
+            })}
+          </Grid>
+        ) : (
+          <Loading>
+            <img src={Logo} />
+          </Loading>
+        )}
       </CollectionWrapper>
     </MetaWrapper>
   );
--- src/containers/Collection/style.js
+++ src/containers/Collection/style.js
@@ -1,28 +1,61 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const CollectionWrapper = styled.div`
-    width:100%;
-    display: flex;
-    justify-content:center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  flex-direction: column;
+`;
+
+const LoadingAnimation = keyframes`
+    0% {transform:translate3d(1000%,0,0) scaleX(-1)}
+
    
+    50% {transform:translate3d(-1000%,0,0) }
+  
+   
+    100%{transform:translate3d(1000%,0,0) }
+`;
 
-`
+
 export const Loading = styled.div`
+display: flex;
+  width: 86vw;
+  justify-content: center;
+  img {
+      
+    animation: ${LoadingAnimation};
+  animation-duration: 6s;
+  animation-iteration-count: infinite;
+    
+  }
+  
+`;
 
-`
+
 export const Grid = styled.div`
-
-    margin-top: 10%;
-    width:86%;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-auto-flow:row;
-    grid-gap: 40px;
-    img {
-        width:100%;
-        height: 100%;
-        &:hover{
-            cursor: pointer;
-        }
+  margin-top: 10%;
+  width: 86%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-flow: row;
+  grid-gap: 40px;
+  position: relative;
+  h2 {
+      color:white;
+      font-size: 18px;
+      font-weight:bold;
+      position: absolute;
+      top:-48px;
+      left: 0;
+     
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    &:hover {
+      cursor: pointer;
     }
-`
+  }
+`;
