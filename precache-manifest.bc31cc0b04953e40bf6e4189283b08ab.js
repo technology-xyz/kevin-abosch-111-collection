d3610193a651aaccd1542ca84c186bbe9f1ca4d4
jeanluciano <jeanluciano@mailbox.org>
2021-06-02 11:50:40 -0500

    Updates

---
+++ precache-manifest.bc31cc0b04953e40bf6e4189283b08ab.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efda9f472f7c2b5f2ca3fc7b69de6bdb",
+    "url": "/kevin-abosch-1111/index.html"
+  },
+  {
+    "revision": "a204bb7882fa53b6dcdf",
+    "url": "/kevin-abosch-1111/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "2832d1d5dac8200271dc",
+    "url": "/kevin-abosch-1111/static/css/main.8d4cc026.chunk.css"
+  },
+  {
+    "revision": "a204bb7882fa53b6dcdf",
+    "url": "/kevin-abosch-1111/static/js/2.ed38ab7a.chunk.js"
+  },
+  {
+    "revision": "1621419735662fcdf567ba531a9b07ec",
+    "url": "/kevin-abosch-1111/static/js/2.ed38ab7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2832d1d5dac8200271dc",
+    "url": "/kevin-abosch-1111/static/js/main.b1733cef.chunk.js"
+  },
+  {
+    "revision": "93115303c651144a9fbc",
+    "url": "/kevin-abosch-1111/static/js/runtime-main.ba81cca1.js"
+  },
+  {
+    "revision": "8f2b457f6ead90ef464ff613c63e2b11",
+    "url": "/kevin-abosch-1111/static/media/BarlowSemiCondensed-Bold.8f2b457f.ttf"
+  },
+  {
+    "revision": "ebd90d8e590842539cb812662b79aca2",
+    "url": "/kevin-abosch-1111/static/media/BarlowSemiCondensed-Regular.ebd90d8e.ttf"
+  },
+  {
+    "revision": "f5adaeba283bedb701b0b61ccf5cd833",
+    "url": "/kevin-abosch-1111/static/media/IconUpload.f5adaeba.svg"
+  },
+  {
+    "revision": "e25e8d509caed454e6cfb0614aa5522b",
+    "url": "/kevin-abosch-1111/static/media/PinkSlab-130.e25e8d50.ttf"
+  },
+  {
+    "revision": "bf0d4e5246466f16614edd53f7e0820c",
+    "url": "/kevin-abosch-1111/static/media/discord.bf0d4e52.svg"
+  },
+  {
+    "revision": "011a9969acea1ebf2f1ae6573172da30",
+    "url": "/kevin-abosch-1111/static/media/email.011a9969.svg"
+  },
+  {
+    "revision": "94439a962117cd756a90749bd3b8133b",
+    "url": "/kevin-abosch-1111/static/media/facebook.94439a96.svg"
+  },
+  {
+    "revision": "079d0a3495e1249a634e80868656ba0b",
+    "url": "/kevin-abosch-1111/static/media/koi-logo.079d0a34.svg"
+  },
+  {
+    "revision": "d532149e789c879f48cdcd327e0fb892",
+    "url": "/kevin-abosch-1111/static/media/linkedin.d532149e.svg"
+  },
+  {
+    "revision": "bd06b9bbf658ff4859c265e7088416d3",
+    "url": "/kevin-abosch-1111/static/media/menu.bd06b9bb.svg"
+  },
+  {
+    "revision": "ac382f6fc37d0a2ee5097f98c316d67f",
+    "url": "/kevin-abosch-1111/static/media/sms.ac382f6f.svg"
+  },
+  {
+    "revision": "1722d10c660284b3591da8ed0a9f17ce",
+    "url": "/kevin-abosch-1111/static/media/telegram.1722d10c.svg"
+  },
+  {
+    "revision": "896abfa5c459c5a896dd25dc1a0be693",
+    "url": "/kevin-abosch-1111/static/media/twitter.896abfa5.svg"
+  }
+]);
