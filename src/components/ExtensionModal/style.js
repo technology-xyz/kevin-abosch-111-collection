765ec8a1174b12f39c5bf0481d9a1f9a1f4fed70
jeanluciano <jeanluciano@mailbox.org>
2021-06-08 15:30:25 -0500

    modal button styled

--- src/components/ExtensionModal/style.js
+++ src/components/ExtensionModal/style.js
@@ -3,17 +3,32 @@ export const ModalWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+  z-index: 4;
+  width: 100vw;
+  height: 100vh;
 `;
 export const Modal = styled.div`
   display: flex;
   flex-direction: column;
   background-color: black;
   color: #fff;
-
+  width: 400px;
   text-align: left;
   align-items: center;
   padding: 8px;
   border: 2px solid #f5b900;
+  button {
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      background-color: var(--yellow);
+      width:150px;
+      height:32px;
+      border:0;
+      font-weight: 700;
+
+  }
 `;
 
 export const ArLink = styled.a``;
